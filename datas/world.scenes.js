c24e72b48223d72c70ad692ca3580a3f78eeba01
段春晓 <www.dcx@gmail.com>
2020-07-08 20:14:10 +0800

    data18 改为ray

--- datas/world.scenes.js
+++ datas/world.scenes.js
@@ -1,5 +1,5 @@
 window.world = window.world || {};
-window.world.currentSceneName = "compound";
+window.world.currentSceneName = "ray";
 window.world.scenes = window.world.scenes || {};
 window.world.scenes = [
 	{cname:'人物模型控制',ename:'avatar'},
@@ -27,7 +27,7 @@ window.world.scenes = [
 	{cname:'好多车',ename:'cars'},
 	{cname:'塔',ename:'data16'},
 	{cname:'绳子',ename:'data17'},
-	{cname:'射线',ename:'data18'},
+	{cname:'射线',ename:'ray'},
 	{cname:'组合体',ename:'compound'},
 	{cname:'关节点对点',ename:'data20'},
 	{cname:'迷宫',ename:'maze'},
